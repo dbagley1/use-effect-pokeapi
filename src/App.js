46feefbe670af375ccb590eedfab27003f415248
Darrian Bagley <dbagle14@gmail.com>
2023-06-03 16:36:03 -0400

    remove useRef PokemonSearch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import PokemonSearch from "./PokemonSearch";
 import Clock from './Clock';
-import PokemonSearchRef from "./PokemonSearchRef";
 
 function App() {
   return (
     <div className="App">
       <Clock />
-      <PokemonSearchRef />
+      <PokemonSearch />
     </div>
   );
 }
