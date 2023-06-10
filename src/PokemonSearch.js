667728446e42599063ad8e729161285fc6a32b67
Darrian Bagley <dbagle14@gmail.com>
2023-06-10 11:46:23 -0400

    fix: purify useEffect setPokemonList refactor: add searchQueryInput

--- src/PokemonSearch.js
+++ src/PokemonSearch.js
@@ -2,30 +2,35 @@ import { useEffect, useState } from "react";
 import Pokemon from './Pokemon';
 
 export default function PokemonSearch() {
-  const [searchInput, setSearchInput] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQueryInput, setSearchQueryInput] = useState('');
   const [pokemonList, setPokemonList] = useState([]);
 
   useEffect(() => {
-    if (searchInput) {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${searchInput}`)
+    if (searchQuery) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${searchQuery}`)
         .then(res => res.json())
-        .then(data => setPokemonList([...pokemonList, data]));
+        .then(data => setPokemonList(pokemonList => [...pokemonList, data]));
     }
-  }, [searchInput]);
+  }, [searchQuery]);
 
   function handleSearchSubmit(e) {
     e.preventDefault();
-    setSearchInput(e.target.elements['search'].value);
+    setSearchQuery(searchQueryInput);
+  }
+
+  function handleSearchInput(e) {
+    setSearchQueryInput(e.target.value);
   }
 
   return (
     <div className="PokemonSearch">
       <form onSubmit={handleSearchSubmit}>
-        <input type="text" id="search" />
+        <input type="text" id="search" onChange={handleSearchInput} value={searchQueryInput} />
       </form>
-      <p>Search Result for '{searchInput}'</p>
+      <p>Search Result for '{searchQuery}'</p>
       <div className="row row-cols-5">
-        {pokemonList.map(pokemon => <Pokemon pokemon={pokemon} />)}
+        {pokemonList.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
       </div>
     </div>
   );
